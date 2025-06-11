4ecc12e352194c3d4b181ad6126d67944c56d498
Fabiano Braga <fabianobragadev@gmail.com>
2025-06-11 10:24:56 -0300

    Gerando coverage de testes

--- src/use-cases/__tests__/register.test.ts
+++ src/use-cases/__tests__/register.test.ts
@@ -33,7 +33,7 @@ describe('Register Use Case', () => {
       password: '123456',
     })
 
-    expect(() =>
+    await expect(() =>
       registerUserCase.execute({
         name: 'John Doe',
         email,
