49c66148fd7345eac6dd112b9f359a2ba2e98182
Fabiano Braga <fabianobragadev@gmail.com>
2025-06-12 18:10:34 -0300

    Caso de uso de academias próximas

--- src/repositories/gyms-repository.ts
+++ src/repositories/gyms-repository.ts
@@ -1,7 +1,13 @@
 import { Gym, Prisma } from 'generated/prisma'
 
+export interface findManyNearByParams {
+  latitude: number
+  longitude: number
+}
+
 export interface GymsRepository {
   findById(id: string): Promise<Gym | null>
+  findManyNearBy(params: findManyNearByParams): Promise<Gym[]>
   create(data: Prisma.GymCreateInput): Promise<Gym>
   searchMany(query: string, page: number): Promise<Gym[]>
 }
