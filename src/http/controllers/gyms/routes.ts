181d29abbe5175ce602406c8fa204e812ac02cba
Fabiano Braga <fabianobragadev@gmail.com>
2025-06-17 12:56:54 -0300

    Outros controllers da academia

--- src/http/controllers/gyms/routes.ts
+++ src/http/controllers/gyms/routes.ts
@@ -1,6 +1,14 @@
 import { VerifyJwt } from '@/http/middlewares/verify-jwt'
 import { FastifyInstance } from 'fastify'
+import { nearby } from './nearby'
+import { search } from './search'
+import { create } from './create'
 
 export async function gymsRoutes(app: FastifyInstance) {
   app.addHook('onRequest', VerifyJwt)
+
+  app.get('/gyms/search', search)
+  app.get('/gyms/nearby', nearby)
+
+  app.post('/gyms', create)
 }
