0bae7b30bfc9043bb3c5c43ec428022c043603c3
Fabiano Braga <fabianobragadev@gmail.com>
2025-06-18 11:53:15 -0300

    Testes E2E de rotas de academias

--- src/http/controllers/gyms/__tests__/create.test.ts
+++ src/http/controllers/gyms/__tests__/create.test.ts
@@ -2,26 +2,27 @@ import request from 'supertest'
 import { afterAll, beforeAll, describe, expect, it } from 'vitest'
 import { app } from '@/app'
 import { createAndAuthenticateUser } from '@/utils/__tests__/create-and-authenticate-user'
-describe('Profile (e2e)', () => {
+describe('Create Gym (e2e)', () => {
   beforeAll(async () => {
     await app.ready()
   })
   afterAll(async () => {
     await app.close()
   })
-  it('should be able to get user profile', async () => {
+  it('should be able to create gym', async () => {
     const { token } = await createAndAuthenticateUser(app)
 
-    const profileResponse = await request(app.server)
-      .get('/me')
+    const response = await request(app.server)
+      .post('/gyms')
       .set('Authorization', `Bearer ${token}`)
-      .send()
+      .send({
+        title: 'javascript',
+        description: 'some',
+        phone: '85981888363',
+        latitude: -27.2092052,
+        longitude: -49.6401091,
+      })
 
-    expect(profileResponse.statusCode).toEqual(200)
-    expect(profileResponse.body.user).toEqual(
-      expect.objectContaining({
-        email: 'johnDoe@example.com',
-      }),
-    )
+    expect(response.statusCode).toEqual(201)
   })
 })
